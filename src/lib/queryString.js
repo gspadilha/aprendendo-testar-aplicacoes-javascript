b5db8af73b2234212e6800f9d93d21740445cce0
Guilherme Padilha <gs.padilha@gmail.com>
2023-04-16 07:58:45 -0300

    feat: implementar library para gerar query string com TDD

---
+++ src/lib/queryString.js
@@ -0,0 +1,5 @@
+module.exports.queryString = (obj) => {
+  return Object.entries(obj)
+    .map((item) => `${item[0]}=${item[1].replaceAll(" ", "%20")}`)
+    .join("&");
+};
---
+++ src/lib/queryString.spec.js
@@ -0,0 +1,23 @@
+const { queryString } = require("./queryString");
+
+describe("Object to Query String", () => {
+  it("deve criar um query valid a partir de um objeto dado", () => {
+    const obj = {
+      name: "Guilherme Padilha",
+      role: "Developer",
+    };
+
+    expect(queryString(obj)).toEqual("name=Guilherme%20Padilha&role=Developer");
+  });
+
+  it("deve criar um query valid a partir de um objeto dado", () => {
+    const obj = {
+      name: "Guilherme dos Santos Padilha",
+      role: "Web Developer",
+    };
+
+    expect(queryString(obj)).toEqual(
+      "name=Guilherme%20dos%20Santos%20Padilha&role=Web%20Developer"
+    );
+  });
+});
