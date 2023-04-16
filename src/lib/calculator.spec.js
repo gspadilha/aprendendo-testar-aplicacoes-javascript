afb935f5e076c76a0a2c874d237ac6eee8a7bc15
Guilherme Padilha <gs.padilha@gmail.com>
2023-04-16 07:48:37 -0300

    feat: jest watch mode, observar alterações nos arquivos

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -3,3 +3,19 @@ const { sum } = require("./calculator");
 it("deve somar 2 e 2 e o resultado deve ser 4", () => {
   expect(sum(2, 2)).toBe(4);
 });
+
+it("deve somar 2 e 2, mesmo um deles sendo string, e o resultado deve ser 4", () => {
+  expect(sum("2", "2")).toBe(4);
+});
+
+it("deve dar erro se não puder realizar a soma", () => {
+  expect(() => sum("", "2")).toThrowError();
+
+  expect(() => sum(2, "")).toThrowError();
+
+  expect(() => sum("", "")).toThrowError();
+
+  expect(() => sum(true, "")).toThrowError();
+
+  expect(() => sum(true, 1)).toThrowError();
+});
